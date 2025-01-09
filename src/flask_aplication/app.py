c7ab93cc262cacee379ac0dd11a6b7617f42679f
nizwant <mateusznizwantowski@gmail.com>
2025-01-09 13:47:19 +0100

    plot vehicles

--- src/flask_aplication/app.py
+++ src/flask_aplication/app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import requests
+from datetime import datetime
 from api_key import api_key
 
 app = Flask(__name__)
+
+# Fetch current vehicles locations 
+url = "https://api.um.warszawa.pl/api/action/busestrams_get"
+params = {
+    "resource_id": "f2e5503e-927d-4ad3-9500-4ab9e55deb59",  # resource id given by the API
+    "apikey": api_key,
+    "type": 1,  # 1 - bus, 2 - tram
+}
+locations = requests.get(url, params=params)
+locations_df = pd.json_normalize(locations.json()["result"])
+locations_df.rename(columns={"Lat": "Latitude", "Lon": "Longitude"}, inplace=True)
+locations_df["Label"] = locations_df["Lines"].astype(str) + "/" + locations_df["Brigade"].astype(str)
+locations_df["Latitude"] = locations_df["Latitude"].astype(str)
+locations_df["Longitude"] = locations_df["Longitude"].astype(str)
+locations_df['Time'] = pd.to_datetime(locations_df['Time'])
+current_time = datetime.now()
+time_diff = current_time - locations_df['Time']
+locations_df = locations_df[time_diff <= pd.Timedelta(seconds=30)]
 
 # Fetch routes from the API
 url_routes = "https://api.um.warszawa.pl/api/action/public_transport_routes"
@@ -49,11 +68,14 @@ def index():
     stops_for_line = pd.DataFrame(stops_for_line)
     stops_for_line = stops_for_line.drop(columns=["odleglosc", "ulica_id", "typ"]).drop_duplicates()
     stops_for_line = pd.merge(stops_for_line, stops_df, left_on=["nr_zespolu", "nr_przystanku"], right_on=["zespol", "slupek"], how="left")
+
+    locations_for_line = locations_df[locations_df["Lines"] == selected_route]
     
     # Convert to dictionary for template
-    locations = stops_for_line.to_dict(orient='records')
+    stops_locations = stops_for_line.to_dict(orient='records')
+    vehicle_locations = locations_for_line.to_dict(orient="records")
 
-    return render_template('map.html', locations=locations, routes=sorted_routes, selected_route=selected_route)
+    return render_template('map.html', stops_locations=stops_locations, routes=sorted_routes, selected_route=selected_route, vehicle_locations=vehicle_locations)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
