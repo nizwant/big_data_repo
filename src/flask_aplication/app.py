b9b52c7245a67cb9c72b5c7d6ad95254ecd1c2ef
nizwant <mateusznizwantowski@gmail.com>
2025-01-09 11:28:27 +0100

    print all stops

--- src/flask_aplication/app.py
+++ src/flask_aplication/app.py
@@ -1,10 +1,37 @@
 from flask import Flask, render_template
+import pandas as pd
+import requests
+from api_key import api_key
 
 app = Flask(__name__)
 
+url = "https://api.um.warszawa.pl/api/action/dbstore_get"
+params = {
+    "id": "ab75c33d-3a26-4342-b36a-6e5fef0a3ac3",  # resource id given by the API
+    "apikey": api_key,
+}
+stops = requests.get(url, params=params)
+
+url = "https://api.um.warszawa.pl//api/action/public_transport_routes"
+params = {
+    "apikey": api_key,
+}
+routes = requests.get(url, params=params)
+
+
+rows = [
+    {entry["key"]: entry["value"] for entry in item["values"]}
+    for item in stops.json()["result"]
+]
+df_transport_location = pd.DataFrame(rows)
+df_transport_location.rename(columns={"szer_geo": "Latitude", "dlug_geo": "Longitude"}, inplace=True)
+df_transport_location["Label"] = df_transport_location["nazwa_zespolu"].astype(str) + " " + df_transport_location["slupek"].astype(str)
+
 @app.route('/')
 def index():
-    return render_template('map.html')
+    # Pass the DataFrame data to the template
+    locations = df_transport_location.to_dict(orient='records')  # Convert the DataFrame to a list of dictionaries
+    return render_template('map.html', locations=locations)
 
 if __name__ == '__main__':
     app.run(debug=True)
