9ccf458e3fd740e07b160a84e25ce3e62815c881
nizwant <mateusznizwantowski@gmail.com>
2025-01-10 20:20:30 +0100

    refactor spark code

--- src/spark/read_location.py
+++ src/spark/read_location.py
@@ -1,24 +1,17 @@
 from pyspark.sql import SparkSession
 
 # Create a Spark session
-spark = SparkSession.builder \
-    .appName("ReadFromHDFS") \
-    .config("spark.driver.host", "localhost") \
+spark = (
+    SparkSession.builder.appName("ReadFromHDFS")
+    .config("spark.driver.host", "localhost")
     .getOrCreate()
+)
 
-# Define the HDFS path to the Parquet files
 parquet_path = "hdfs://hdfs-namenode:8020/big_data/locations_test.parquet"
-
-# Read the Parquet data from HDFS
 parquet_df = spark.read.parquet(parquet_path)
 
-# Show the data to the console
 parquet_df.show(truncate=False)
-
-# Optionally, you can print the schema of the dataframe
 parquet_df.printSchema()
-
 parquet_df.describe().show()
 
-# Stop the Spark session after processing
 spark.stop()
